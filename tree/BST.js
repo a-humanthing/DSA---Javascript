a10a93d67cf9fb0c891437db6e36c0a8e024cd67
a-humanthing <arunkrishnakt2002@gmail.com>
2024-12-13 13:38:42 +0530

    Added tree deletion and BFS

--- tree/BST.js
+++ tree/BST.js
@@ -108,12 +108,85 @@ class BST {
             console.log(paret.value)
         }
     }
+
+    levelOrder(){
+        const queue = []
+        queue.push(this.root)
+        while(queue.length){
+            const curr = queue.shift()
+            console.log(curr.value)
+            if(curr.left){
+                queue.push(curr.left)
+            }
+            if(curr.right){
+                queue.push(curr.right)
+            }
+        }
+    }
+
+    min(parent){
+        while(parent.left){
+            parent = parent.left
+        }
+        return parent.value;
+    }
+
+    max(parent){
+       if(!parent.right){
+           return parent.value
+    }else{
+        return this.max(parent.right)
+       }
+    }
+
+    deleteNode(parent,value){
+
+        if(value<parent.value){
+            parent.left = this.deleteNode(parent.left,value)
+        }else if(value>parent.value){
+            parent.right = this.deleteNode(parent.right,value)
+        }else{
+            if(!parent.left && !parent.right){
+                return null;
+            }
+
+            if(!parent.left){
+                return parent.right
+            }else if(!parent.right){
+                return parent.left
+            }
+
+            parent.value = this.min(parent.right)
+            parent.right = this.deleteNode(parent.right,parent.value)
+        }
+        return parent;
+       
+    }
+
+    // delete(value){
+    //     if(this.isEmpty()){
+    //         console.log("Tree is empty")
+    //         return;
+    //     }
+    //     let parent = this.root;
+    //     parent = this.deleteNode(parent,value)
+    // }
+
+    delete(value) {
+        if (this.isEmpty()) {
+            console.log("Tree is empty");
+            return;
+        }
+        this.root = this.deleteNode(this.root, value);
+    }
 }
 
 const bst = new BST()
-bst.insert(2)
+bst.insert(10)
 bst.insert(5)
-bst.insert(1)
+bst.insert(15)
+bst.insert(3)
+// bst.insert(7)
 console.log(bst.isEmpty())
 console.log(bst.getSize())
 console.log(bst.search("arun"))
@@ -123,3 +196,10 @@ console.log("inorder")
 bst.inOrder(bst.root)
 console.log("postorder")
 bst.postOrder(bst.root)
+console.log("LevelOrder")
+bst.levelOrder()
+
+bst.delete(5)
+console.log("min=",bst.min(bst.root))
+bst.levelOrder()
+console.log("max=",bst.max(bst.root))
